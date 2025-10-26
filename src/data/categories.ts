7806927e6f76a144d2875d5eb32a8b258d5dda88
Jordy17-gif <flameorbx8@gmail.com>
2025-10-26 04:09:00 -0400

    Update categories.ts

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,7 +1,7 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
+	'Olympia 2025',
+	'Prague Pro 2025',
 	'Category 3',
 	'Category 4',
 	'Category 5'
